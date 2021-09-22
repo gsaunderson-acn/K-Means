a1d7981106423217ef9854db09a8bb87e46466fc
Gaguro <bereket.gaguro@accenture.com>
2021-09-22 13:29:13 -0700

    =added random generated large data

---
+++ kmeans_random_data/common.py
@@ -0,0 +1,43 @@
+from sklearn.datasets import make_blobs
+
+import numpy as np
+import pandas as pd
+
+X, y = make_blobs(n_samples = 10 * 10**6, n_features = 50,
+                  centers = 10, cluster_std = 0.2,
+                  center_box = (-10.0, 10.0), random_state = 777)
+
+X_df = pd.DataFrame(X)
+y_df = pd.DataFrame(y)
+
+STATS = '#, median, mean, std_dev, min_time, max_time, quantile_10, quantile_90'
+
+def get_test_data_df(X,size: int = 1):
+    """Generates a test dataset of the specified size""" 
+    num_rows = len(X)
+    test_df = X.copy()
+    while num_rows < size:
+        test_df = test_df.append(test_df)
+        num_rows = len(test_df)
+
+    return test_df[:size].reset_index(drop = True)
+
+def calculate_stats(time_list):
+    """Calculate mean and standard deviation of a list"""
+    time_array = np.array(time_list)
+
+    median = np.median(time_array)
+    mean = np.mean(time_array)
+    std_dev = np.std(time_array)
+    max_time = np.amax(time_array)
+    min_time = np.amin(time_array)
+    quantile_10 = np.quantile(time_array, 0.1)
+    quantile_90 = np.quantile(time_array, 0.9)
+    
+    basic_key = ["median","mean","std_dev","min_time","max_time","quantile_10","quantile_90"]
+    basic_value = [median,mean,std_dev,min_time,max_time,quantile_10,quantile_90]
+
+    dict_basic = dict(zip(basic_key, basic_value))
+
+    
+    return pd.DataFrame(dict_basic, index = [0])
---
+++ kmeans_random_data/kmeans_all/daal_kmeans.py
@@ -0,0 +1,46 @@
+from timeit import default_timer as timer
+
+# import xgboost as xgb
+import daal4py as d4p
+import numpy as np
+import pandas as pd
+
+import common
+
+d4p.daalinit()
+NUM_LOOPS = 10
+
+print("Computing for Kmeans with Daal")
+
+init_alg = d4p.kmeans_init(nClusters = 10, fptype = "float", method = "randomDense")
+centroids = init_alg.compute(common.X_df).centroids
+
+def run_inference(num_observations:int = 1000):
+    """Run xgboost for specified number of observations"""
+    # Load data
+    test_df = common.get_test_data_df(X=common.X_df,size = num_observations)
+    num_rows = len(test_df)
+    ######################
+    print("_______________________________________")
+    print("Total Number of Rows", num_rows)
+    run_times = []
+    inference_times = []
+    for _ in range(NUM_LOOPS):
+        
+        start_time = timer()
+        
+        alg = d4p.kmeans(nClusters = 10, maxIterations = 0, fptype = "float", accuracyThreshold = 0,
+                         assignFlag = False)
+        result = alg.compute(test_df, centroids)
+        
+        end_time = timer()
+
+        total_time = end_time - start_time
+        run_times.append(total_time*10e3)
+
+        inference_time = total_time*(10e6)/num_rows
+        inference_times.append(inference_time)
+
+    return_elem = common.calculate_stats(inference_times)
+    print(num_observations, ", ", return_elem)
+    return return_elem
